3c1a1295976b746f2e625cd24a09deaef95b2436
Poznamenany <32431877+Poznamenany@users.noreply.github.com>
2019-01-19 12:29:24 +0100

    Support of arrays

--- DLL_Delphi/ExtAI.pas
+++ DLL_Delphi/ExtAI.pas
@@ -57,12 +57,26 @@ end;
 // Test all callbacks and events
 procedure TExtAI.Event1(aID: ui32);
 var
-  testVar: ui8;
+  testVar, K: ui8;
+  pMap: pui32;
+  mapLen: si32;
+  Map: ui32Arr;
 begin
   writeln('    TExtAI: Event1, class fID: ' + IntToStr(ID) + '; parameter aID: ' + IntToStr(aID)); // Show event 1
   Actions.Action1(11,22); // Check callback
   testVar := States.State1(22);
   writeln('    TExtAI: Event1, class fID: ' + IntToStr(ID) + '; testVar: ' + IntToStr(testVar)); // Show test var from State 1
+  // Get array (pointer to first element) from Main program and copy memory so we can work with it
+  pMap := nil;
+  if States.State2(pMap, mapLen) then
+  begin
+    SetLength(Map,mapLen);
+    Move(pMap^, Map[0], SizeOf(Map[0]) * Length(Map));
+    write('    TExtAI: Event1, class fID: ' + IntToStr(ID) + '; log array:'); // Show values of array
+    for K := Low(Map) to High(Map) do
+      write(' ' + IntToStr(Map[K]));
+    writeln('');
+  end;
 end;
 
 
